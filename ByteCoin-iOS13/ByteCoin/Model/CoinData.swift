313069c273e20e862b519d4d2462bdb4c1b5ef6d
washind14 <43079358+washind14@users.noreply.github.com>
2021-12-26 22:29:20 -0700

    Add files via upload

---
+++ ByteCoin-iOS13/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Desha Washington on 6/11/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
